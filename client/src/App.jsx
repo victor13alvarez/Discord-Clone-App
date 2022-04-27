24627353d9aed9906e98d2986f4969f0c3c71871
victor13alvarez <55243827+victor13alvarez@users.noreply.github.com>
2022-04-28 00:20:48 +0200

    Client-Side-Atuh Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,15 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import { Cookies } from "universal-cookie";
 
-import { ChannelContainer, ChannelListContainer } from "./components";
+import { ChannelContainer, ChannelListContainer, Auth } from "./components";
 import "./App.css";
 
 const apiKey = "98fqgbsyqg88";
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
 const App = () => {
+  if (!authToken) return <Auth></Auth>;
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from "react";
+import Cookies from "universal-cookie";
+import axios from "axios";
+
+const initialState = {
+  fullName: "",
+  username: "",
+  phoneNumber: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const Auth = () => {
+  const [form, setForm] = useState(initialState);
+  const [isSignup, setIsSignup] = useState(true);
+  const [validPassword, setValidPassword] = useState(true);
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  const switchMode = () => {
+    setIsSignup((prevIsSignUp) => !prevIsSignUp);
+  };
+
+  useEffect(() => {
+    //here you will have correct value in form since this gets update in every render
+    console.log(form.password, form.confirmPassword);
+    setValidPassword(form.password === form.confirmPassword);
+  }, [form]);
+
+  return (
+    <div className="auth__form-container">
+      <div className="auth__form-container_fields">
+        <div className="auth__form-container_fields-content">
+          <p>{isSignup ? "Sign Up" : "Sign In"} </p>
+          <form onSubmit={handleSubmit}>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="fullName">Full Name</label>
+                <input
+                  name="fullName"
+                  type="text"
+                  placeholder="Full Name"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="username">Username</label>
+              <input
+                name="username"
+                type="text"
+                placeholder="Username"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="phoneNumber">Phone Number</label>
+                <input
+                  name="phoneNumber"
+                  type="number"
+                  placeholder="Phone Number"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            {/* PROFILE PICTURE IF WANTED */}
+            {/* {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="avatar">Profile Picture</label>
+                <input
+                  name="avatar"
+                  type="image"
+                  placeholder="Profile Picture"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )} */}
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="password">Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="Confirm Password"
+                  onChange={handleChange}
+                  required
+                  style={{
+                    color:
+                      validPassword || form.confirmPassword === ""
+                        ? "rgb(61, 79, 88)"
+                        : "red",
+                    background:
+                      validPassword || form.confirmPassword === ""
+                        ? ""
+                        : "rgba(236, 149, 149, 0.300)",
+                  }}
+                />
+              </div>
+            )}
+            <div className="auth__form-container_fields-content_button">
+              <button disabled={!validPassword}>
+                {isSignup ? "Sign Up" : "Sign In"}
+              </button>
+            </div>
+          </form>
+          <div className="auth__form-container_fields-account">
+            <p>
+              {isSignup
+                ? "Already have an account?"
+                : "Don't have an account yet?"}
+              <span onClick={switchMode}>
+                {isSignup ? "Sign In!" : "Sign Up!"}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
